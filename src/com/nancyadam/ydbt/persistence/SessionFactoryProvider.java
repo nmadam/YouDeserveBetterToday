0302dc07b393ffcadfe105811c1f544ea3d2f7cb
Nancy Meister <nmeister1@madisoncollege.edu>
2016-02-28 17:22:02 -0600

    Added hibernate config and sesionfactory files

---
+++ src/com/nancyadam/ydbt/persistence/SessionFactoryProvider.java
@@ -0,0 +1,34 @@
+package com.nancyadam.ydbt.persistence;
+
+import org.hibernate.Hibernate;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+
+/**
+ * This file provides a SessionFactory for use with DAOS using Hibernate
+ * Created by Nancy on 2/28/2016.
+ * @author nancyadam
+ * @version 1.0 02/28/16
+ */
+public class SessionFactoryProvider {
+
+    private static SessionFactory sessionFactory;
+
+    public static void createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                configuration.getProperties()). buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            createSessionFactory();
+        }
+        return sessionFactory;
+    }
+}
