512ff790574905c58281da830a6c0eeaed4d572f
nmadam <adam.nancym@gmail.com>
2016-05-12 17:33:54 -0500

    added user book dao testing

---
+++ Tests/com/nancyadam/ydbt/persistence/UserBookDaoTest.java
@@ -0,0 +1,82 @@
+package com.nancyadam.ydbt.persistence;
+
+import com.nancyadam.ydbt.entity.UserBook;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Student on 5/12/2016.
+ */
+public class UserBookDaoTest {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllRatings() throws Exception {
+
+        UserBookDao userBookDao = new UserBookDao();
+        List<UserBook> userBooks = userBookDao.getAllRatings();
+        assertTrue(userBooks.size() > 0);
+    }
+
+    @Test
+    public void testDeleteUserBook() throws Exception {
+
+        UserBookDao userBookDao = new UserBookDao();
+
+        UserBook userBook = new UserBook();
+
+        userBook.setComment("This should be deleted");
+        userBook.setRating("5 star");
+        userBook.setUserId(16);
+        userBook.setBookId(16);
+        userBookDao.addUserBook(userBook);
+
+        userBookDao.deleteUserBook(userBook);
+
+        UserBook userBook1 = userBookDao.selectUserBook(16);
+
+        assertNull(userBook1);
+    }
+
+
+    @Test
+    public void testAddUserBook() throws Exception {
+
+        UserBookDao userBookDao = new UserBookDao();
+
+        UserBook userBook = new UserBook();
+
+        userBook.setComment("This will insert if the test is successful");
+        userBook.setRating("5 star");
+        userBook.setUserId(16);
+        userBook.setBookId(16);
+        userBookDao.addUserBook(userBook);
+
+        List<UserBook> userBookRatings = userBookDao.getAllRatings();
+
+        int numberOfBooks = 0;
+        for (UserBook userBookRating : userBookRatings) {
+            if (userBookRating.getBookId() == 16) {
+                numberOfBooks++;
+            }
+            assertTrue(numberOfBooks == 1);
+        }
+        userBookDao.deleteUserBook(userBook);
+    }
+}
--- src/com/nancyadam/ydbt/persistence/UserBookDao.java
+++ src/com/nancyadam/ydbt/persistence/UserBookDao.java
@@ -1,7 +1,10 @@
 package com.nancyadam.ydbt.persistence;
+import com.nancyadam.ydbt.entity.User;
 import com.nancyadam.ydbt.entity.UserBook;
+import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.ArrayList;
@@ -11,6 +14,7 @@ import java.util.List;
  * Created by Student on 5/6/2016.
  */
 public class UserBookDao {
+    private final Logger log = Logger.getLogger(this.getClass());
 
     public List<UserBook> getAllRatings() {
         List<UserBook> userBooks = new ArrayList<UserBook>();
@@ -21,9 +25,45 @@ public class UserBookDao {
         return userBooks;
     }
 
+    public UserBook selectUserBook(int bookId) {
+        UserBook userBook = null;
+        Session session = SessionFactoryProvider1.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            userBook = (UserBook) session.get(UserBook.class, bookId);
+            tx.commit();
+            log.info("Found the User Book with Id: " + bookId);
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            log.error(e);
+        } finally {
+            session.close();
+        }
+        return userBook;
+    }
+
+    public void deleteUserBook(UserBook userBook) {
+        Session session = SessionFactoryProvider1.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.delete(userBook);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            log.error(e);
+        } finally {
+            session.close();
+        }
+    }
+
     public void addUserBook(UserBook userBook) {
         Session session = SessionFactoryProvider1.getSessionFactory().openSession();
         Transaction tx = null;
+
 
         try {
             tx = session.beginTransaction();
@@ -31,7 +71,7 @@ public class UserBookDao {
             tx.commit();
         } catch (HibernateException e) {
             if (tx!=null) tx.rollback();
-            e.printStackTrace();
+            log.error(e);
         } finally {
             session.close();
         }
